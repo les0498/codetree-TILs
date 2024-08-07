dfd2dff509970e5030922365b70ef1d88fef03b5
eunseo <92281453+les0498@users.noreply.github.com>
2024-08-07 14:17:14 +0900

    Title: 369 게임, Time: 132ms, Memory: 73MB, Status: Passed - CodeTree

--- 240807/369 게임/369-game.py
+++ 240807/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 result = []
 for i in range(1,n+1):
-    if i % 3 == 0:
+    if i % 3 == 0 or any(digit in '369' for digit in str(i)):
         result.append('0')
     else :
         result.append(str(i))
