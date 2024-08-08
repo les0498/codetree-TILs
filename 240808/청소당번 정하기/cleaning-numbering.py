b6b4975b0a83b0238fcea7718832a51eed50e59c
eunseo <92281453+les0498@users.noreply.github.com>
2024-08-08 10:54:17 +0900

    Title: 청소당번 정하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240808/청소당번 정하기/cleaning-numbering.py
+++ 240808/청소당번 정하기/cleaning-numbering.py
@@ -1,12 +1,15 @@
-days = int(input()) 
-cnt1 = 0
-cnt2 = 0
-cnt3 = 0 
-for i in range(1, days):
-    if i % 3 == 0:
-        cnt2 += 1
-    elif i % 2 == 0:
-        cnt1 += 1
-    elif i % 12 == 0:
-        cnt3 += 1
-print(cnt1, cnt2, cnt3)
+n = int(input())
+
+classroom = 0
+hallway = 0
+restroom = 0
+
+for day in range(1, n + 1):
+    if day % 12 == 0:
+        restroom_cleaning += 1
+    elif day % 3 == 0:
+        hallway += 1
+    elif day % 2 == 0:
+        classroom += 1
+
+print(classroom, hallway, restroom)
