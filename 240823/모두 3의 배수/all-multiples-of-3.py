1add1595e0dc314434a05933b1dc731dcba7007d
eunseo <92281453+les0498@users.noreply.github.com>
2024-08-23 15:17:23 +0900

    Title: 모두 3의 배수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/모두 3의 배수/all-multiples-of-3.py
+++ 240823/모두 3의 배수/all-multiples-of-3.py
@@ -1,10 +1,9 @@
-satisfied = False 
+satisfied = True
 for i in range (5) :
     num = int(input())
-    if num % 3 == 0:
-        satisfied = True
-    else: 
+    if num % 3 != 0:
         satisfied = False
+        break
 if satisfied:
     print("1")
 else:
