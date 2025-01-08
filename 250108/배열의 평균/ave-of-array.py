9c031184aafcc261df46ebfee77b18cc5d91d32b
eunseo <92281453+les0498@users.noreply.github.com>
2025-01-08 23:08:03 +0900

    Title: 배열의 평균, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250108/배열의 평균/ave-of-array.py
@@ -0,0 +1,11 @@
+arr = [list(map(int, input().split())) for _ in range(2)]
+
+horizontal_avg = [sum(row) / len(row) for row in arr]
+
+vertical_avg = [sum(arr[row][col] for row in range(2)) / 2 for col in range(4)]
+
+total_avg = sum(sum(row) for row in arr) / (2 * 4)
+
+print(" ".join(f"{x:.1f}" for x in horizontal_avg))
+print(" ".join(f"{x:.1f}" for x in vertical_avg))
+print(f"{total_avg:.1f}")
